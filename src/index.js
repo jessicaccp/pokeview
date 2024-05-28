fea7b9815b9dd20419373731e1af5d5dc20f9729
Jessica Patricio <jessicacacau@gmail.com>
2024-05-28 19:11:50 -0300

    change page title

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/css/index.css';
 
 const root = document.getElementById('root');
 const container = ReactDOM.createRoot(root);
+
+document.title = 'PokéView';
 container.render(<App />);

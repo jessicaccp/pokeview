db086a02498bdabf6d7d5b96d378691413b3bd74
Jessica Patricio <jessicacacau@gmail.com>
2024-06-05 14:33:22 -0300

    add select and sort

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -6,34 +6,15 @@ function PokemonCard(props) {
             <div id='card-info'>
                 <h2>#{props.id} {props.name}</h2>
                 <ul id='card-types'>{props.types}</ul>
-                <ul id='card-stats'>{props.stats}</ul>
+                <ul id='card-stats'>
+                    <li>Height: {props.height}</li>
+                    <li>Weight: {props.weight}</li>
+                    {props.stats}
+                </ul>
             </div>
             <div id='card-img'>
                 <img src={props.image} alt={props.name} />
             </div>
-
-            {/* <div class='pokemon-info'>
-                <div class='pokemon-id'>{props.id}</div>
-                <div class='pokemon-name'>{props.name}</div>
-                <div class='pokemon-base-experience'>{props.base_experience}</div>
-                <div class='pokemon-height'>{props.height}</div>
-                <div class='pokemon-is-default'>{props.is_default}</div>
-                <div class='pokemon-order'>{props.order}</div>
-                <div class='pokemon-weight'>{props.weight}</div>
-                <div class='pokemon-abilities'>{props.abilities}</div>
-                <div class='pokemon-past-abilities'>{props.past_abilities}</div>
-                <div class='pokemon-forms'>{props.forms}</div>
-                <div class='pokemon-game-indices'>{props.game_indices}</div>
-                <div class='pokemon-held-items'>{props.held_items}</div>
-                <div class='pokemon-location-area-encounters'>{props.location_area_encounters}</div>
-                <div class='pokemon-moves'>{props.moves}</div>
-                <div class='pokemon-past-types'>{props.past_types}</div>
-                <div class='pokemon-sprites'>{props.sprites}</div>
-                <div class='pokemon-cries'>{props.cries}</div>
-                <div class='pokemon-species'>{props.species}</div>
-                <div class='pokemon-stats'>{props.stats}</div>
-                <div class='pokemon-types'>{props.types}</div>
-            </div> */}
         </section>
     );
 }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -3,73 +3,120 @@ import PokemonCard from "./PokemonCard";
 
 function PokemonList() {
     const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
-    let pageContent = '';
 
     const [pokemonData, setPokemonData] = useState([]);
     const [limit, setLimit] = useState(12);
     const [offset, setOffset] = useState(0);
     const [count, setCount] = useState(0);
+    const [order, setOrder] = useState('id');
 
-    // get api data
+    // order select
+    const selectOptions = [
+        {value: 'id', title: ''},
+        {value: 'height', title: ''},
+        {value: 'weight', title: ''},
+        {value: 'order', title: ''},
+        {value: 'name', title: ''}
+    ];
+
+    const selectOptionsList = selectOptions.map(
+        (option, key) =>
+            <option key={key} value={option.value}>{option.value}</option>
+    );
+
+    function handleSelectOrder(event) {
+        setOrder(event.target.value);
+    }
+    // end
+
+    // pagination buttons
+    function handlePrevButton() {
+        if (offset > 0) {
+            setOffset((offset - limit < 0) ? 0 : offset - limit);
+        }
+    }
+
+    function handleNextButton() {
+        if (offset < count) {
+            setOffset(offset + limit);
+        }
+    }
+    // end
+
+    // pokemon data
     useEffect(() => {
-        setPokemonData([])
-        fetch(`${apiUrl}?limit=${limit}&offset=${offset}`)
-        .then(response => response.json())
+        setPokemonData([]);
+        fetch(`${apiUrl}?limit=${count}`)
+        .then(response =>
+             response.json())
         .then(data => {
+            if (count !== 0) {
+                data.results.forEach(
+                    result => {
+                        fetch(`${result.url}`)
+                        .then(resultResponse =>
+                            resultResponse.json())
+                        .then(resultData =>
+                            setPokemonData(prev =>
+                                [...prev, resultData]));
+                    }
+                );
+            }
             setCount(data.count);
-            data.results.forEach(
-                result => {
-                    fetch(`${result.url}`)
-                    .then(resultResponse => resultResponse.json())
-                    .then(resultData => setPokemonData(prev => [...prev, resultData]));
-                }
-            );
         })
         .catch(error => console.error(error.message));
-    }, [apiUrl, limit, offset]);
+    }, [apiUrl, limit, offset, count]);
+    // end
 
-    function getPageContent() {
+    // page content
+    let pageContent = '';
+
+    function setPageContent() {
         pageContent = pokemonData.map(
             pokemon => <PokemonCard
                 key={pokemon.id}
                 id={pokemon.id} 
                 name={pokemon.name}
+                order={pokemon.order}
                 image={pokemon.sprites.front_default}
+                height={pokemon.height}
+                weight={pokemon.weight}
                 stats={pokemon.stats.map(
-                    (stat, key) => <li key={key}>{stat.stat.name}: {stat.base_stat}</li>
+                    (stat, key) =>
+                         <li key={key}>{stat.stat.name}: {stat.base_stat}</li>
                 )}
                 types={pokemon.types.map(
-                    (type, key) => <li key={key}>{type.type.name}</li>
+                    (type, key) =>
+                         <li key={key}>{type.type.name}</li>
                 )} />
         );
-        pageContent.sort((a, b) => a.key - b.key);
-    }
-
-    function goToPreviousPage() {
-        if (offset > 0) {
-            if (offset - limit < 0) {
-                setOffset(0);
+        pageContent.sort(
+            function (a,b) {
+                if (a.props.name < b.props.name)
+                    return -1;
+                if (a.props.name > b.props.name)
+                    return 1;
+                return 0;
             }
-            else {
-                setOffset(offset - limit);
-            }
-        }
+        );
+        
+        // pageContent.sort((a, b) => a.props.name - b.props.name);
     }
 
-    function goToNextPage() {
-        if (offset < count) {
-            setOffset(offset + limit);
-        }
-    }
-
-    getPageContent();
+    setPageContent();
+    // end
+    
     return(
         <>
-            <div id='filter-by'></div>
-            <div id='order-by'></div>
-            <div id='pagination'>
-                <button className='prev' onClick={goToPreviousPage}></button>
-                <button className='next' onClick={goToNextPage}></button>
+            <div id='options'>
+                <div id='filter-by'></div>
+                <div id='order-by'>
+                    <select default='id' name='order-by' id='order-select' onChange={handleSelectOrder}>{selectOptionsList}</select>
+                </div>
+                <div id='pagination'>
+                    <button className='prev' onClick={handlePrevButton}></button>
+                    <button className='next' onClick={handleNextButton}></button>
+                </div>
             </div>
             <div id='pokemon-list'>
                 {pageContent}

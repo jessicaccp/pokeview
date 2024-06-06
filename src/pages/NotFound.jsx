17e8065a898b0a0bfc110d69fd4a804d3d0473e3
Jessica Patricio <jessicacacau@gmail.com>
2024-06-06 20:23:47 -0300

    change not found component to page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+
+function NotFound() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <p>No results found.</p>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default NotFound;

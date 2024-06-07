0b3cba450748684568083270c54acdba313adc00
Jessica Patricio <jessicacacau@gmail.com>
2024-06-06 23:28:32 -0300

    add material ui code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./assets/index.css";
+// import "./assets/index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
 
 const root = document.getElementById("root");
 const container = ReactDOM.createRoot(root);
 
 container.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline enableColorScheme>
+      <App />
+    </CssBaseline>
   </React.StrictMode>
 );

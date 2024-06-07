e673fcbfc877c7382ab3eaa616bfa0a243473c93
Jessica Patricio <jessicacacau@gmail.com>
2024-06-07 12:16:39 -0300

    add request data from api

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Pokemons() {
-  return <></>;
+  const [fetchUrl, setFetchUrl] = useState(`https://pokeapi.co/api/v2/pokemon`);
+  const [count, setCount] = useState(0);
+  const [alreadyCount, setAlreadyCount] = useState(false);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [gotError, setGotError] = useState(false);
+
+  // get total count of pokémon from pokéapi
+  useEffect(() => {
+    if (alreadyCount === false) {
+      fetch(fetchUrl)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            setGotError(true);
+            throw new Error(`Error while fetching: ${response.status}`);
+          }
+        })
+        .then((data) => {
+          if (data) {
+            setCount(data.count);
+            setAlreadyCount(true);
+          } else {
+            setGotError(true);
+            throw new Error(`Error while setting Count value`);
+          }
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [alreadyCount, fetchUrl]);
+
+  // get all pokémon data from pokéapi
+  useEffect(() => {
+    if (alreadyCount) {
+      setIsLoading(true);
+      fetch(`${fetchUrl}?limit=${count}`)
+        .then((response) => response.json())
+        .then((data) => setPokemonData(data))
+        .then(() => setIsLoading(false))
+        .then(console.log("got data"))
+        .catch((error) => console.error(error));
+    }
+  }, [count]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (gotError) return <p>Got error</p>;
+
+  return (
+    <>
+      <div id="pokemon-card-list"></div>
+    </>
+  );
 }

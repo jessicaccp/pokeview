3dbcfa9462a880373d1a6073f2663ebd60dbfdbe
Jessica Patricio <jessicacacau@gmail.com>
2024-06-14 11:25:35 -0300

    fix search results not showing

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,22 +3,74 @@ import { useNavigate } from "react-router-dom";
 import SearchResult from "./SearchResult";
 
 export default function Search(props) {
+  const [searchKeywords, setSearchKeywords] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [resultCards, setResultCards] = useState([]);
   const navigate = useNavigate();
 
-  // Changes the content displayed at the bottom of the
-  // page every time the results are modified
-  useEffect(() => {
-    if (searchResults) {
-      setResultCards([]);
-      searchResults.forEach((result, key) => {
-        setResultCards((prev) => [
-          ...prev,
-          <SearchResult result={result} key={key} />,
-        ]);
+  // Search by id
+  // Detects all the integers in the string (type: null | Array)
+  // For each number, tries to find an pokémon with this number as an id
+  function searchById(keywords) {
+    const numbers = keywords.match(/\d+/g);
+    let matchList = [];
+
+    if (numbers) {
+      numbers.forEach((number) => {
+        const pokemon = props.pokemonData.find(
+          (pokemon) => pokemon.id === Number(number),
+        );
+
+        if (pokemon) {
+          if (matchList.includes(pokemon) === false) {
+            matchList.push(pokemon);
+          }
+        }
       });
     }
+
+    return matchList;
+  }
+
+  // Search by name (at least 3 letters)
+  // Detects all substrings in the string
+  // For each substring of at least 3 letters, checks if
+  // there is a pokémon name that includes the substring
+  function searchByName(keywords) {
+    const substrings = keywords.split(" ");
+    const matchList = [];
+  
+    substrings.forEach((substring) => {
+      if (substring.length >= 3) {
+        props.pokemonData.forEach((pokemon) => {
+          if (pokemon.name.includes(substring)) {
+            if (matchList.includes(pokemon) === false) {
+              matchList.push(pokemon);
+            }
+          }
+        });
+      }
+    });
+
+    return matchList;
+  }
+
+  function handleResultCards(results) {
+    let cardList = [];
+
+    if (results) {
+      cardList = results.map((result, key) => <SearchResult result={result} key={key} />);
+    }
+
+    return cardList;
+  }
+
+  useEffect(() => {
+    setSearchResults([...searchById(searchKeywords), ...searchByName(searchKeywords)]);
+  }, [searchKeywords]);
+
+  useEffect(() => {
+    setResultCards(handleResultCards(searchResults));
   }, [searchResults]);
 
   // Gets a random pokémon from pokemonData (type: object)
@@ -30,7 +82,6 @@ export default function Search(props) {
   // Navigates to the individual page of the random pokémon
   function handleRandomClick() {
     const pokemon = getRandomPokemon();
-    // console.log(pokemon.name);
     navigate(`/pokemon/${pokemon.name}`);
   }
 
@@ -39,52 +90,17 @@ export default function Search(props) {
     if (event.target.value === "") navigate(`/`);
     else navigate(`/?search=${event.target.value}`);
 
-    setSearchResults([]);
-
-    // Search by id
-    // Detects all the integers in the string (type: null | Array)
-    // For each number, tries to find an pokémon with this number as an id
-    const numbers = event.target.value.match(/\d+/g);
-    if (numbers) {
-      numbers.forEach((number) => {
-        const pokemon = props.pokemonData.find(
-          (pokemon) => pokemon.id === Number(number)
-        );
-
-        if (pokemon) {
-          if (searchResults.includes(pokemon) === false) {
-            setSearchResults((prev) => [...prev, pokemon]);
-          }
-        }
-      });
-    }
-
-    // Search by name (at least 3 letters)
-    // Detects all substrings in the string
-    // For each substring of at least 3 letters, checks if
-    // there is a pokémon name that includes the substring
-    const substrings = event.target.value.split(" ");
-    substrings.forEach((substring) => {
-      if (substring.length >= 3) {
-        props.pokemonData.forEach((pokemon) => {
-          if (pokemon.name.includes(substring)) {
-            if (searchResults.includes(pokemon) === false) {
-              setSearchResults((prev) => [...prev, pokemon]);
-            }
-          }
-        });
-      }
-    });
+    setSearchKeywords(event.target.value);
 
     // Center the search form if there are no results
-    const searchComponents = document.getElementById("search-component");
-    if (searchComponents) {
-      if (event.target.value === "") {
-        searchComponents.style.justifyContent = "center";
-      } else {
-        searchComponents.style.justifyContent = "normal";
-      }
-    }
+    // const searchComponents = document.getElementById("search-component");
+    // if (searchComponents) {
+    //   if (event.target.value === "") {
+    //     searchComponents.style.justifyContent = "center";
+    //   } else {
+    //     searchComponents.style.justifyContent = "normal";
+    //   }
+    // }
   }
 
   return (
@@ -97,7 +113,7 @@ export default function Search(props) {
             name="search"
             id="search-input"
             onChange={handleSearchChange}
-            autoFocus
+            // autoFocus
           />
           <div id="search-buttons">
             {/* <input type="button" value="Search" id="search-button" /> */}
